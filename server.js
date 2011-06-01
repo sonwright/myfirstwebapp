fe05bae3f95fd29630dcdaeaa7e4d5fbd039cab1
Sonny Gomez <sonnyg95@gmail.com>
2011-05-31 22:04:41 -1000

    Add routing to the webapp

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
   var server = http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		
-		route(pathname);
-		
     response.writeHead(200, {"Content-type": "text/html"});
-    response.write("Hello World\n");
+		var content = route(handle, pathname);
+		response.write(content);
+    //response.write("Hello World\n");
     response.end();
   });
 
